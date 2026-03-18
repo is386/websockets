e9cd69de14ad8df9eee97a40953d0f2d0b0844b8
is386 <singh.indervir89@gmail.com>
2026-03-17 23:05:55 -0400

    add basic request parsing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -30,4 +31,32 @@ func main() {
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 	fmt.Println("Connected to client")
+
+	buffer := make([]byte, 2048)
+
+	for {
+		// This blocks until something is received
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		request := strings.Split(string(buffer[:n]), "\r\n")
+		requestLine := strings.Split(request[0], " ")
+		headers := make(map[string]string)
+
+		for i := 1; i < len(request); i++ {
+			header := strings.Split(request[i], ": ")
+			if len(header) < 2  {
+				continue
+			}
+			headers[header[0]] = header[1]
+		}
+		
+		if requestLine[0] != "GET" {
+			fmt.Println("Error: method of request MUST be GET")
+			return
+		}
+	}
 }
